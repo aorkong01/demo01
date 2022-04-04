1a10056393fd022d728578a72cfb62b61e42b42c
gypsophila <13388103271@163.com>
2022-04-04 11:09:52 +0800

    冲突合并在hot-fix分支上的修改

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,7 +3,8 @@ public class HelloWorld{
 	public static void main(String[] args){
 
 		System.out.println("Hello World!");
-		System.out.println("呵呵");
+		System.out.println("haha");
 		System.out.println("在hot-fix上添加内容");
+		System.out.println("在hot-fix分支上又添加的内容");
 	}
 }
